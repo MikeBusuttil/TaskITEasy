72749fc5da53ae492fc70814bcd52d7beafb87a2
Mike Busuttil <mikebusuttil@gmail.com>
2024-11-18 21:58:31 -0500

    top nav with icons

--- ui/app/root.jsx
+++ ui/app/root.jsx
@@ -8,6 +8,8 @@ import styles from "./tailwind.css?url";
 export const links = () => [
   { rel: "stylesheet", href: styles },
 ];
+import User from "./user.svg?react"
+import Menu from "./hamburger.svg?react"
 
 export default function App() {
   return (
@@ -21,6 +23,15 @@ export default function App() {
         <Links />
       </head>
       <body>
+        <div className="flex flex-row h-12 w-full border-b border-gray-100 border-opacity-10">
+          <div className="object-cover m-2 rounded-full hover:bg-gray-200 focus:bg-gray-200 focus:outline-none">
+            <Menu className="h-full p-2 fill-gray-500" />
+          </div>
+          <div className="flex-grow"></div>
+          <div className="object-cover m-2 rounded-full hover:bg-gray-200 focus:bg-gray-200 focus:outline-none">
+            <User className="h-full p-2 fill-gray-500" />
+          </div>
+        </div>
         <h1 className="text-3xl font-bold underline">Hello our world!!👋</h1>
         <Outlet />
 
